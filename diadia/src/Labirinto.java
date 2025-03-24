a0958c08fb4bae19ed87bbfa2fa08f514051cab3
Cangurosa <Danieldellaposta43@gmail.com>
2025-03-24 16:04:52 +0100

    versione base DiaDia + testing
    Funzioni test aggiunte, da implementare Labirinto e Giocatore

---
+++ diadia/src/Labirinto.java
@@ -0,0 +1,14 @@
+
+
+/**
+* Ha la responsabilità di creare il labirinto, di memorizzare la stanza
+* iniziale (entrata) e quella finale (uscita)
+ *
+ * @author  Daniel 605587
+ * @see Stanza
+ * @version base
+ */
+
+public class Labirinto {
+
+}

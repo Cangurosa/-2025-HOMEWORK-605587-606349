bb0bfa4dc3c1d343f731dc82239783092382f3f2
Daniel Della Posta <Danieldellaposta43@gmail.com>
2025-03-30 13:45:34 +0200

    Update Giocatore.java

--- it/uniroma3/diadia/giocatore/Giocatore.java
+++ it/uniroma3/diadia/giocatore/Giocatore.java
@@ -1,58 +1,87 @@
 package it.uniroma3.diadia.giocatore;
 
-import diadia.Partita;
 import it.uniroma3.diadia.attrezzi.Attrezzo;
 
 /**
  * Si occupa di gestire i cfu della partita e gli
  * oggetti contenuti nella borsa
  *
- * @author  Daniel 605587, Gabriel 606349
+ * @author  Daniel Della Posta 605587, Gabriel Garcia Ramos 606349
  * @see Partita
  * @see Borsa
- * @version base
+ * @version finale
  */
 
 public class Giocatore {
-	static final private int CFU_INIZIALI = 20;
-	private Borsa borsa;
-	private int cfu;
-	
+	static final private int CFU_INIZIALI = 20; //CFU di partenza
+	private Borsa borsa; //variabile borsa del giocatore
+	private int cfu; //variabile cfu
+
+	//costruttore
 	public Giocatore()
 	{
 		this.cfu = CFU_INIZIALI;
 		this.borsa = new Borsa();
 	}
 	
-	/*Metodo per ottenere il numero di cfu */
+	/**
+ 	*
+  	*Metodo che ritorna il numero di cfu
+   	*
+	*@return numero di cfu
+ 	*/
 	public int getCfu()
 	{
 		return this.cfu;
 	}
 	
-	/*Metodo che imposta i cfu del giocatore*/
+	/**
+ 	*
+  	*Metodo che ritorna imposta i cfu
+   	*@param cfu cfu passati da dinamiche esterne
+ 	*/
 	public void setCfu(int cfu) {
 		this.cfu = cfu;		
 	}	
 	
-	/*Metodo che mostra la borsa del giocatore*/
+	/**
+ 	*
+  	*Metodo che mostra la borsa del giocatore
+   	*
+	*@return borsa del giocatore
+ 	*/
 	public Borsa getBorsa()
 	{
 		return this.borsa;
 	}
 	
-	/*Metodo che aggiunge un attrezzo in borsa*/
+	/**
+ 	*
+  	*Metodo che aggiunge un attrezzo
+   	*@param attrezzo attrezzo da aggiungere
+ 	*/
 	public void addAttrezzo(Attrezzo attrezzo)
 	{
 		 this.borsa.addAttrezzo(attrezzo);
 	}
-	
+
+	/**
+ 	*
+  	*Metodo che verifica l'esistenza di un attrezzo in base al nome
+   	*@param nomeAttrezzo nome dell'attrezzo richiesto
+	*@return true se esiste un attrezzo con lo stesso nome
+ 	*/
 	public boolean hasAttrezzo(String nomeAttrezzo)
 	{
 		return this.borsa.hasAttrezzo(nomeAttrezzo);
 	}
 	
-	/*Metodo che ritorna il peso della borsa*/
+	/**
+ 	*
+  	*Metodo che ritorna il peso della borsa
+   	*
+	*@return peso della borsa
+ 	*/
 	public int getPesoBorsa()
 	{
 		return borsa.getPeso();

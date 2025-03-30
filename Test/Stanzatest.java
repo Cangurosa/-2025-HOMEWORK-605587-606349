fc11dfb75da9d513868874d76ac333db658effde
Daniel Della Posta <Danieldellaposta43@gmail.com>
2025-03-30 13:48:16 +0200

    Update Stanzatest.java

--- Test/Stanzatest.java
+++ Test/Stanzatest.java
@@ -1,12 +1,8 @@
-package diadia;
-
-
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.ambienti.*;
 import it.uniroma3.diadia.attrezzi.Attrezzo;
 
 //*I metodi principali sono getNome(), impostaStanzaAdiacente() e addAttrezzo()*//

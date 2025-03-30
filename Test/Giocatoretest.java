914e9f107b3afb568eb7ccb0c2ea04e41cec7891
Daniel Della Posta <Danieldellaposta43@gmail.com>
2025-03-30 13:18:35 +0200

    Add files via upload

---
+++ Test/Giocatoretest.java
@@ -0,0 +1,35 @@
+package diadia;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+import it.uniroma3.diadia.giocatore.Giocatore;
+
+public class Giocatoretest {
+	private Giocatore giocatore;
+	private Attrezzo attrezzo;
+	
+	@BeforeEach
+	public void setUp()
+	{
+		giocatore = new Giocatore();
+		attrezzo = new Attrezzo("spada", 5);
+		
+	}
+	
+	@Test
+	public void testGetCfu()
+	{
+		giocatore.setCfu(5);
+		assertEquals(5, giocatore.getCfu());
+	}
+	
+	@Test
+	public void testHasAttrezzo()
+	{
+		giocatore.addAttrezzo(attrezzo);
+		assertTrue(giocatore.hasAttrezzo(attrezzo.getNome()));
+	}
+}

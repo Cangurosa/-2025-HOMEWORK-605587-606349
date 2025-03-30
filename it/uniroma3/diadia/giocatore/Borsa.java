09cf739e2aede11fe4c287eac157d18279570a22
Daniel Della Posta <Danieldellaposta43@gmail.com>
2025-03-30 13:38:42 +0200

    Update Borsa.java

--- it/uniroma3/diadia/giocatore/Borsa.java
+++ it/uniroma3/diadia/giocatore/Borsa.java
@@ -1,7 +1,5 @@
-package diadia;
-
-
-
+package it.uniroma3.diadia.giocatore;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
 
 public class Borsa {
 	public final static int DEFAULT_PESO_MAX_BORSA = 10;
